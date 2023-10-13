8e1c68ab94bcf4a28d51479b6b9b9eca1b544884
Harrison Le <hakile@ucsc.edu>
2023-10-13 01:15:20 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,20 @@ const gameName = "Harrison's Game";
 
 document.title = gameName;
 
+let counter: number = 0;
+
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
 const button = document.createElement("button");
-button.innerHTML = "Button";
+button.innerHTML = "🥞";
+button.addEventListener("click", () => {
+  counter++;
+  countDiv.innerHTML = `${counter} pancakes`;
+});
 app.append(button);
+
+const countDiv: HTMLDivElement = document.createElement("div");
+countDiv.innerHTML = `${counter} pancakes`;
+app.append(countDiv);
