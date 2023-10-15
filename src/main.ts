eb4685bfd7e872997986300f98305358971ea83e
Harrison Le <hakile@ucsc.edu>
2023-10-14 18:13:48 -0700

    Step 8 complete

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,7 @@ function step(timeStamp: number) {
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Harrison's Game";
+const gameName = "Harrison's Pancake Clicker";
 
 document.title = gameName;
 
@@ -44,44 +44,51 @@ button.innerHTML = "🥞";
 button.addEventListener("click", () => {
   counter++;
 });
+button.style.fontSize = "3em";
 app.append(button);
 
+app.append(document.createElement("div"));
+
 const upgrade1 = document.createElement("button");
-upgrade1.innerHTML = `+ A (🥞x${uCosts[0].toFixed(2)})`;
+upgrade1.innerHTML = `+ Pancake Shop (🥞x${uCosts[0].toFixed(2)})`;
 upgrade1.disabled = true;
 upgrade1.addEventListener("click", () => {
   countRate += 0.1;
   counter -= uCosts[0];
   uCounts[0]++;
   uCosts[0] *= 1.15;
-  upgrade1.innerHTML = `+ A (🥞x${uCosts[0].toFixed(2)})`;
-  u1Count.innerHTML = `${uCounts[0]} A`;
+  upgrade1.innerHTML = `+ Pancake Shop  (🥞x${uCosts[0].toFixed(2)})`;
+  u1Count.innerHTML = `${uCounts[0]} Pancake Shops`;
 });
 app.append(upgrade1);
 
+app.append(document.createElement("div"));
+
 const upgrade2 = document.createElement("button");
-upgrade2.innerHTML = `+ B (🥞x${uCosts[1].toFixed(2)})`;
+upgrade2.innerHTML = `+ Pancake Factory (🥞x${uCosts[1].toFixed(2)})`;
 upgrade2.disabled = true;
 upgrade2.addEventListener("click", () => {
   countRate += 2;
   counter -= uCosts[1];
   uCounts[1]++;
   uCosts[1] *= 1.15;
-  upgrade2.innerHTML = `+ B (🥞x${uCosts[1].toFixed(2)})`;
-  u2Count.innerHTML = `${uCounts[1]} B`;
+  upgrade2.innerHTML = `+ Pancake Factory (🥞x${uCosts[1].toFixed(2)})`;
+  u2Count.innerHTML = `${uCounts[1]} Pancake Factories`;
 });
 app.append(upgrade2);
 
+app.append(document.createElement("div"));
+
 const upgrade3 = document.createElement("button");
-upgrade3.innerHTML = `+ C (🥞x${uCosts[2].toFixed(2)})`;
+upgrade3.innerHTML = `+ Pancake Reactor (🥞x${uCosts[2].toFixed(2)})`;
 upgrade3.disabled = true;
 upgrade3.addEventListener("click", () => {
   countRate += 50;
   counter -= uCosts[2];
   uCounts[2]++;
   uCosts[2] *= 1.15;
-  upgrade3.innerHTML = `+ C (🥞x${uCosts[2].toFixed(2)})`;
-  u3Count.innerHTML = `${uCounts[2]} C`;
+  upgrade3.innerHTML = `+ Pancake Reactor (🥞x${uCosts[2].toFixed(2)})`;
+  u3Count.innerHTML = `${uCounts[2]} Pancake Reactors`;
 });
 app.append(upgrade3);
 
@@ -94,16 +101,16 @@ rateDiv.innerHTML = `${countRate.toFixed(1)} 🥞/s`;
 app.append(rateDiv);
 
 const u1Count: HTMLDivElement = document.createElement("div");
-u1Count.innerHTML = `${uCounts[0]} A`;
+u1Count.innerHTML = `${uCounts[0]} Pancake Shops`;
 app.append(u1Count);
 
 const u2Count: HTMLDivElement = document.createElement("div");
-u2Count.innerHTML = `${uCounts[1]} B`;
+u2Count.innerHTML = `${uCounts[1]} Pancake Factories`;
 app.append(u2Count);
 
 const u3Count: HTMLDivElement = document.createElement("div");
-u3Count.innerHTML = `${uCounts[2]} C`;
+u3Count.innerHTML = `${uCounts[2]} Pancake Reactors`;
 app.append(u3Count);
 
 window.requestAnimationFrame(step);
-console.log("Step 7");
+console.log("Step 8");
